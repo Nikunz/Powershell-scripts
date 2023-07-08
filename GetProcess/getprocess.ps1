51d1e74eee693a5ed55600da328a70a9f2dee3a4
Nikunz <123695337+Nikunz@users.noreply.github.com>
2023-07-07 21:04:14 -0400

    Update getprocess.ps1

--- GetProcess/getprocess.ps1
+++ GetProcess/getprocess.ps1
@@ -1,1 +1,18 @@
+$ProcessName = "WhatsApp"
+$Process = Get-Process -Name $ProcessName
 
+if ($Process) {
+    $ProcessInfo = [PSCustomObject]@{
+        Name = $Process.Name
+        ID = $Process.Id
+        CPU = $Process.CPU
+        Memory = $Process.WorkingSet
+        StartTime = $Process.StartTime
+    }
+
+    $ProcessInfo | Export-Csv -Path ".\ProcessInfo.csv" -NoTypeInformation 
+    Write-Host "Process information exported to ProcessInfo.csv"
+
+} else {
+    Write-Host "No process found with name $ProcessName"
+}
