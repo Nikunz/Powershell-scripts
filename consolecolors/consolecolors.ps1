6b21239a52f75959854d7b3370f78452a0f28a18
Nikunz <123695337+Nikunz@users.noreply.github.com>
2023-07-12 13:41:36 -0400

    Create consolecolors.ps1

---
+++ consolecolors/consolecolors.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+	Lists all console colors
+.DESCRIPTION
+	This PowerShell script lists all available console colors.
+#>
+
+try {
+	$Colors = [Enum]::GetValues([ConsoleColor])
+	""
+	"Color          As Foreground  As Background"
+	"-----          -------------  -------------"
+	foreach($Color in $Colors) {
+		$Color = "$Color              "
+		$Color = $Color.substring(0, 15)
+		write-host -noNewline "$Color"
+		write-host -noNewline -foregroundcolor $Color "$Color"
+		write-host -noNewline -backgroundcolor $Color "$Color"
+		write-host ""
+	}
+	exit 0 # success
+} catch {
+	"Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
